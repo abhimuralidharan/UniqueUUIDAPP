d9f1489d79096122e92fcfecf28b90eac7b00366
Abhilash KM <abhimuralidharan1992@gmail.com>
2017-06-21 22:38:51 +0530

    First commit

--- UniqueUUIDAPP/ViewController.swift
+++ UniqueUUIDAPP/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  UniqueUUIDAPP
+//  UUIDTEST
 //
 //  Created by Apple on 21/06/17.
 //  Copyright © 2017 Abhilash. All rights reserved.
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        print("\(UUID())")    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func UUID() -> String {
+        
+        let bundleName = Bundle.main.infoDictionary!["CFBundleName"] as! String
+        let accountName = Bundle.main.bundleIdentifier! + String(Bundle.main.bundleIdentifier!.characters.reversed()) // just one extra layer of protection.. :p
+        
+        var applicationUUID = SAMKeychain.password(forService: bundleName, account: accountName)
+        
+        if applicationUUID == nil {
+            
+            applicationUUID = (UIDevice.current.identifierForVendor?.uuidString)!
+            
+            // Save applicationUUID in keychain without synchronization
+            let query = SAMKeychainQuery()
+            query.service = bundleName
+            query.account = accountName
+            query.password = applicationUUID
+            query.synchronizationMode = SAMKeychainQuerySynchronizationMode.no
+            
+            do {
+                try query.save()
+            } catch let error as NSError {
+                print("SAMKeychainQuery Exception: \(error)")
+            }
+        }
+        
+        return applicationUUID!
+    }
 }
 
